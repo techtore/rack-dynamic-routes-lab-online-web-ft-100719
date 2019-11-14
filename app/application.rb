7c177717ba4207944e537c2c470750908d77532b
Victoria Hale <vic.hale74@gmail.com>
2019-11-14 22:30:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,10 +6,10 @@ class Application
     resp = Rack::Response.new 
     req = Rack::Request.new(env)
     
-    if req.path.match("/items")
+    if req.path.match("/items/")
       item_name = req.path.split("/items/").last
       item = @@items.find{|item| item.name == item_name}
-    else 
+    elsif 
       resp.write "Route not found"
       resp.status = 404
     else 
