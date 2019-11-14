7662a03e91d0130bf745c80657575a617f0185b9
Victoria Hale <vic.hale74@gmail.com>
2019-11-14 22:31:03 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if req.path.match("/items/")
       item_name = req.path.split("/items/").last
       item = @@items.find{|item| item.name == item_name}
-    elsif 
+    elsif item.nil?
       resp.write "Route not found"
       resp.status = 404
     else 
