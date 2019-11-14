8d14108507bbb04d6edad6f264f0f72ecb30de22
Victoria Hale <vic.hale74@gmail.com>
2019-11-14 22:29:20 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
     if req.path.match("/items")
       item_name = req.path.split("/items/").last
       item = @@items.find{|item| item.name == item_name}
-      resp.write @@items.price
+      resp.write item.price
     else 
       resp.write "Route not found"
       resp.status = 404
