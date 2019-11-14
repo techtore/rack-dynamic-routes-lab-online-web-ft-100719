cacdf66f29ee729b1b5af80f4cffc1cd3d2ce218
Victoria Hale <vic.hale74@gmail.com>
2019-11-14 22:33:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if req.path.match("/items/")
       item_name = req.path.split("/items/").last
       item = @@items.find{|item| item.name == item_name}
-    elsif item.nil?
+    if item.nil?
       resp.write "Item not found"
       resp.status = 404
     else 
