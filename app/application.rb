98ba4c25970c1cd14d1931b1ffa190015b59b308
Victoria Hale <vic.hale74@gmail.com>
2019-11-14 22:32:15 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,10 +10,13 @@ class Application
       item_name = req.path.split("/items/").last
       item = @@items.find{|item| item.name == item_name}
     elsif item.nil?
-      resp.write "Route not found"
+      resp.write "Item not found"
       resp.status = 404
     else 
       resp.write item.price
+    else 
+      resp.write "Route not found"
+      resp.status = 404
     end
     
     resp.finish
